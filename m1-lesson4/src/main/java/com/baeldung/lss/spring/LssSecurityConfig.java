fc45bf7eb2c4b3ccc1f659fb967bac9277f08921
Gaetano_Piazzolla <gae.piaz@gmail.com>
2023-04-03 10:33:48 +0200

    remaining lessons

--- m1-lesson4/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m1-lesson4/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -1,16 +1,24 @@
 package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class LssSecurityConfig {
+
+    private PasswordEncoder passwordEncoder;
 
     public LssSecurityConfig() {
         super();
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     //
@@ -18,21 +26,17 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off 
         auth.
-            inMemoryAuthentication().
-            withUser("user").password("pass").
-            roles("USER");
+                inMemoryAuthentication().passwordEncoder(passwordEncoder).
+                withUser("user").password(passwordEncoder.encode("pass")).
+                roles("USER");
     } // @formatter:on
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception { // @formatter:off
-        http
-        .authorizeRequests()
-                .antMatchers("/delete/**").hasRole("ADMIN")
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {// @formatter:off
+        return http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/delete/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
-        
-        .and()
-        .formLogin()
-        ;
+        ).formLogin().and().build();
     } // @formatter:on
 
 }
--- m1-lesson5/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m1-lesson5/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -1,16 +1,24 @@
 package com.baeldung.lss.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
-public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class LssSecurityConfig {
+
+    private PasswordEncoder passwordEncoder;
 
     public LssSecurityConfig() {
         super();
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     //
@@ -18,22 +26,22 @@ public class LssSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // @formatter:off 
         auth.
-            inMemoryAuthentication().
-            withUser("user").password("pass").
+            inMemoryAuthentication().passwordEncoder(passwordEncoder).
+            withUser("user").password(passwordEncoder.encode("pass")).
             roles("USER");
     } // @formatter:on
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception { // @formatter:off
-        http
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {// @formatter:off
+        return http
         .authorizeRequests()
                 .anyRequest().authenticated()
         
         .and()
         .formLogin().
             loginPage("/login").permitAll().
-            loginProcessingUrl("/doLogin")
-        ;
+            loginProcessingUrl("/doLogin").and().build();
+
     } // @formatter:on
 
 }
